00c145ce9abf532289661d9e2d3abb95e6fa23f0
Andrew <andrew@Capelagames.com.au>
2024-02-21 12:52:11 +1100

    Added mesk links and surface

---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public Vector3 deltaPosition;
+    public float speed = 5f;
+    public float waitTime = 3f;
+
+    private Vector3 _closedPosition;
+    private Vector3 _openPosition;
+
+    void Start()
+    {
+        _closedPosition = transform.position;
+        _openPosition = transform.position + deltaPosition;
+    }
+    void Update()
+    {
+        if(Time.time > waitTime) 
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _openPosition, speed * Time.deltaTime);
+        }
+    }
+}
